c49405896e4a95e99f800469bddca4424d1b7a9e
mohamedeliwa <mohamed.av11@gmail.com>
2020-07-23 03:17:37 +0200

    Home page redesigned, and made more responsive. Login and Signup pages are built, .babelrc file added to solve styling break due to uages of styled-components package

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,15 +1,137 @@
-
 import Link from "next/link";
-import {Navbar, Container} from "react-bootstrap";
+import { Navbar, Container, Nav, Dropdown } from "react-bootstrap";
+import styled from "styled-components";
 
+/**
+ * if the user not authenticated
+ **/
+const StyledNav = styled(Nav)`
+
+  // background-color: #eee;
+  flex-basis:50%;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  flex-wrap: nowrap;
+  flex-direction: row;
+  // span{
+  //   border: 1px solid black;
+    
+  // }
+  @media screen and (max-width: 768px){
+    & span {
+      margin: 5px;
+    }
+  }
+
+`;
 export default () => {
   return (
-   
-    <Navbar expand="lg" bg="white" variant="dark">
-    <Container >
-        <Navbar.Brand><Link href="/"><div className="text-secondary" style={{cursor: "pointer"}}>Pocket-Post</div></Link></Navbar.Brand>
-    </Container>
-  </Navbar>
+    <Container>
+      <Navbar  collapseOnSelect  bg="white" expand="md" className="d-flex justify-content-between">
 
+        <Navbar.Brand className="flex-fill">
+          <Link href="/">
+            <div className="text-secondary" style={{ cursor: "pointer" }}>
+              Pocket-Post
+            </div>
+          </Link>
+        </Navbar.Brand>
+
+          <StyledNav /*bg="light"*/ /*className="flex-fill bg-light  justify-content-end"*/>
+            <Nav.Link as="span">
+              <Link href="/accounts/login">
+                <div className="text-primary" style={{ cursor: "pointer" }}>
+                  Log In
+                </div>
+              </Link>
+            </Nav.Link>
+            <Nav.Link as="span" >
+              <Link href="/accounts/signup">
+                <div className="text-primary" style={{ cursor: "pointer" }}>
+                  Sign up
+                </div>
+              </Link>
+            </Nav.Link>
+          </StyledNav>
+
+      </Navbar>
+    </Container>
   );
 };
+
+/**
+ * when the user logged in
+ */
+
+// const StyledToggle = styled(Dropdown.Toggle)`
+//   outline: none;
+//   border: none;
+//   background-color: white;
+//   &:focus,
+//   &:active,
+//   &:hover,
+//   &:disabled {
+//     border: none !important;
+//     background-color: white !important;
+//     outline: none !important;
+//     box-shadow: none !important;
+//   }
+// `;
+
+// export default () => {
+//   return (
+//     <Container>
+//       <Navbar
+//         collapseOnSelect
+//         bg="white"
+//         expand="md"
+//         className="d-flex justify-content-between"
+//       >
+//         <Navbar.Brand className="flex-fill">
+//           <Link href="/">
+//             <div className="text-secondary" style={{ cursor: "pointer" }}>
+//               Pocket-Post
+//             </div>
+//           </Link>
+//         </Navbar.Brand>
+//         <StyledNav /*bg="light" className="flex-fill flex-nowrap justify-content-end"*/>
+//           <Nav.Link as="span">
+//             <Link href="/accounts/login">
+//               <div className="text-primary" style={{ cursor: "pointer" }}>
+//                 New Post
+//               </div>
+//             </Link>
+//           </Nav.Link>
+
+//           <Dropdown>
+//             {/* <Dropdown.Toggle  className="border-0 rounded bg-white">
+//               <img src="/profile.png"  style={{width: "30px"}} />
+//             </Dropdown.Toggle> */}
+//             <StyledToggle /*className="border-0 rounded bg-white"*/>
+//               <img src="/profile.png" style={{ width: "30px" }} />
+//             </StyledToggle>
+
+//             <Dropdown.Menu>
+//               <Dropdown.Item>
+//                 <Link href="/accounts/login">
+//                   <div>
+//                     Prfolie
+//                   </div>
+//                 </Link>
+//               </Dropdown.Item>
+//               <Dropdown.Divider />
+//               <Dropdown.Item >
+//               <Link href="/accounts/login">
+//               <div>
+//                    Logout
+//               </div>
+//               </Link>
+//               </Dropdown.Item>
+//             </Dropdown.Menu>
+//           </Dropdown>
+//         </StyledNav>
+//       </Navbar>
+//     </Container>
+//   );
+// };
---
+++ pages/accounts/login.js
@@ -0,0 +1,72 @@
+import styled from "styled-components";
+import { Container, Form, Button } from "react-bootstrap";
+
+
+const Login = styled(Container)`
+//background-color: #eee;
+  min-height: 70vh;
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+`;
+
+const StyledForm = styled(Form)`
+   // background-color: white;
+    // background-color: #eee;
+    width: 350px;
+    padding: 1rem;
+    padding-bottom: 2rem;
+    margin-bottom: 10px;
+    border-radius: 5px;
+    // border: 1px solid lightgrey;
+    border: 1px solid #cfcfcf;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    input{
+        width: 250px;
+    }
+    button {
+        width: 250px;
+    }
+
+`;
+
+const FormHeader = styled.h1`
+    margin: 22px auto 50px;
+`;
+
+const login = () => {
+  return (
+    <Login>
+        <StyledForm className="bg-light">
+            <FormHeader>Pocket-Post</FormHeader>
+
+            <Form.Group controlId="formBasicEmail">
+            <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+            <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            
+            <Button variant="primary" type="submit" block>
+            Log In
+            </Button>
+            
+        </StyledForm>
+
+        <Form.Group className="bg-light text-dark p-2" style={{border: "1px solid #eee"}} controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="remember me" />
+        </Form.Group>
+        
+    </Login>
+  );
+};
+
+export default login;
