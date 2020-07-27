ffa96ed09ea7f57abedacce19e7bb431ec0cbc73
mohamedeliwa <mohamed.av11@gmail.com>
2020-07-27 08:10:16 +0200

    editorjs wrapper component created

---
+++ components/editorjsWrapper.js
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+import Head from "next/head";
+
+export default class extends Component {
+  constructor(props) {
+    super(props);
+    this.editor;
+    this.handleSaving = this.handleSaving.bind(this);
+  }
+  shouldComponentUpdate() {
+    return false;
+  }
+  componentDidMount() { 
+    
+      this.editor = new EditorJS({
+        /**
+         * Id of Element that should contain Editor instance
+         */
+        holder: "editorjs",
+        autofocus: true,
+        initialBlock: "header",
+        // placeholder?: string|false,
+        data: this.props.data,
+        tools: {
+          header: Header,
+          image: {
+            // Docs : https://github.com/editor-js/image
+            class: ImageTool,
+            config: {
+              endpoints: {
+                byFile: "http://localhost:8008/uploadFile", // Your backend file uploader endpoint
+                byUrl: "http://localhost:8008/fetchUrl", // Your endpoint that provides uploading by Url
+              },
+            },
+          },
+          list: List,
+          checklist: Checklist,
+          quote: Quote,
+          delimiter: Delimiter,
+          Marker: Marker,
+         
+        }, // end of tools
+        onReady: this.props.onReady,
+        onChange: this.props.onChange,
+      });
+  }
+
+  handleSaving(e) {
+    e.preventDefault();
+    this.editor
+      .save()
+      .then((data) => console.log(data))
+      .catch((error) => {
+        console.log("Saving failed: ", error);
+      });
+  }
+  render() {
+    return (
+      <div className="bg-white">
+        <Head>
+          <script src="https://cdn.jsdelivr.net/npm/@editorjs/editorjs@latest"></script>
+          <script src="https://cdn.jsdelivr.net/npm/@editorjs/header@latest"></script>
+          <script src="https://cdn.jsdelivr.net/npm/@editorjs/image@latest"></script>
+          <script src="https://cdn.jsdelivr.net/npm/@editorjs/list@latest"></script>
+          <script src="https://cdn.jsdelivr.net/npm/@editorjs/checklist@latest"></script>
+          <script src="https://cdn.jsdelivr.net/npm/@editorjs/delimiter@latest"></script>
+          <script src="https://cdn.jsdelivr.net/npm/@editorjs/quote@latest"></script>
+          <script src="https://cdn.jsdelivr.net/npm/@editorjs/marker@latest"></script>
+      
+        </Head>
+        <div id="editorjs"></div>
+
+        <button onClick={this.handleSaving}>Save</button>
+      </div>
+    );
+  }
+}
