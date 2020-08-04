b18bcbae55d7af907e2b81d4b7e721f85f5f5434
mohamedeliwa <mohamed.av11@gmail.com>
2020-08-04 19:37:24 +0200

    making login and sign up pages doesn't appear to logged in users

--- pages/accounts/signup.js
+++ pages/accounts/signup.js
@@ -1,60 +1,67 @@
+import { useEffect, useContext } from "react";
+import { useRouter } from "next/router";
 import styled from "styled-components";
 import { Container, Form, Button } from "react-bootstrap";
+import { AuthContext } from "../../context/AuthContext";
 
 const SignUp = styled(Container)`
-    //background-color: #eee;
-    min-height: 70vh;
-    padding: 1rem;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+  //background-color: #eee;
+  min-height: 70vh;
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 
 const StyledForm = styled(Form)`
-
-    // background-color: #eee;
-    width: 350px;
-    padding: 1rem;
-    padding-bottom: 2rem;
-    margin-bottom: 10px;
-    border-radius: 5px;
-    // border: 1px solid lightgrey;
-    border: 1px solid #cfcfcf;
-    text-align: center;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    input{
-        width: 300px;
-    }
-    button {
-        width: 300px;
-    }
+  // background-color: #eee;
+  width: 350px;
+  padding: 1rem;
+  padding-bottom: 2rem;
+  margin-bottom: 10px;
+  border-radius: 5px;
+  // border: 1px solid lightgrey;
+  border: 1px solid #cfcfcf;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  input {
+    width: 300px;
+  }
+  button {
+    width: 300px;
+  }
 `;
 
 const FormHeader = styled.h1`
-    margin: 22px auto 50px;
+  margin: 22px auto 50px;
 `;
 
 const signup = () => {
+  const router = useRouter();
+  const { isAuthenticated } = useContext(AuthContext);
+  useEffect(() => {
+    if(isAuthenticated) router.push("/")
+
+  }, [])
+
   return (
     <SignUp>
       <StyledForm className="bg-light">
+        <FormHeader>Pocket-Post</FormHeader>
 
-         <FormHeader>Pocket-Post</FormHeader>
-
-        {/* for first name */ }
+        {/* for first name */}
         <Form.Group controlId="formBasicFirstName">
           <Form.Control type="text" placeholder="First name" />
         </Form.Group>
 
-        {/* for last name */ }
+        {/* for last name */}
         <Form.Group controlId="formBasicLastName">
           <Form.Control type="text" placeholder="Last name" />
         </Form.Group>
-
 
         <Form.Group controlId="formBasicEmail">
           <Form.Control type="email" placeholder="Email" />
@@ -70,7 +77,6 @@ const signup = () => {
         <Button variant="primary" type="submit" block>
           Sign up
         </Button>
-
       </StyledForm>
     </SignUp>
   );
