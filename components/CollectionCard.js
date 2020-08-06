4dc6b5b634cf2aae2887f9e121987aa1b2854e61
mohamedeliwa <mohamed.av11@gmail.com>
2020-08-06 23:50:25 +0200

    adding conrtols for editting collections and their content

--- components/CollectionCard.js
+++ components/CollectionCard.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { Container, Badge, Image, Jumbotron, Button } from "react-bootstrap";
 import { TiFlag } from "react-icons/ti";
+import { MdDelete } from "react-icons/md";
 import styled from "styled-components";
 const StyledCollectionCard = styled(Container)`
   //background-color: lightyellow;
@@ -11,6 +12,24 @@ const StyledCollectionCard = styled(Container)`
 `;
 
 const CollectionCard = (props) => {
+  const readerControls = (
+    <>
+      <span style={{ margin: "0px 4px" }}>
+        <Badge variant="light">{props.collection.postCount}</Badge>Posts
+      </span>
+      <span style={{ margin: "0px 4px" }}>
+        <Badge variant="light">{props.collection.likesCount}</Badge>likes
+      </span>
+      <span style={{ margin: "0px 0px 0px 15px", cursor: "pointer" }}>
+        report <TiFlag />
+      </span>
+    </>
+  );
+  const ownerControls = (
+    <>
+      <MdDelete style={{color: "red", margin: "auto auto auto 20px", fontSize: "20px", cursor: "pointer"}}/>
+    </>
+  );
   return (
     <Jumbotron fluid className="bg-white">
       <StyledCollectionCard>
@@ -20,19 +39,11 @@ const CollectionCard = (props) => {
 
         <div className="collection-info">
           <h1>{props.collection.name}</h1>
-          <p>
-            {props.collection.desc}
-          </p>
+          <p>{props.collection.desc}</p>
+          {/* Controls */}
           <div>
-            <span style={{ margin: "0px 4px" }}>
-              <Badge variant="light">{props.collection.postCount}</Badge>Posts
-            </span>
-            <span style={{ margin: "0px 4px" }}>
-              <Badge variant="light">{props.collection.likesCount}</Badge>likes
-            </span>
-            <span style={{ margin: "0px 0px 0px 15px", cursor: "pointer" }}>
-              report <TiFlag />
-            </span>
+          {readerControls}
+          {/* {ownerControls} */}
           </div>
           <br />
           <Button onClick={props.fn}>View Collection</Button>
--- components/CollectionContent.js
+++ components/CollectionContent.js
@@ -1,28 +1,71 @@
 import Link from "next/link";
 import { Container, ListGroup, Button } from "react-bootstrap";
-import {IoMdArrowRoundBack} from "react-icons/io";
-import styled from 'styled-components';
+import { IoMdArrowRoundBack } from "react-icons/io";
+import { MdKeyboardArrowDown, MdKeyboardArrowUp } from "react-icons/md";
+import { TiDeleteOutline } from "react-icons/ti";
+import styled from "styled-components";
 
 
-const StyledListItem= styled(ListGroup.Item)`
+const PostName = styled.span`
   cursor: pointer;
   &:hover {
     color: #007bff;
   }
-
+`;
+const Controls = styled.span`
+  font-size: 23px;
+  & > * {
+    cursor: pointer;
+    margin: auto 5px;
+  }
 `;
 
 const CollectionContent = (props) => {
   return (
     <Container>
-      <Button variant="outline-primary" onClick={props.fn}><IoMdArrowRoundBack /> Back</Button>
+      <Button variant="outline-primary" onClick={props.fn}>
+        <IoMdArrowRoundBack /> Back
+      </Button>
       <br />
       <br />
       <ListGroup variant="flush">
-        <Link href="/posts/[id]" as='/posts/hello'><StyledListItem> 1st post in the collection </StyledListItem></Link>
-        <Link href="/posts/[id]" as='/posts/hello'><StyledListItem> 2nd post in the collection </StyledListItem></Link>
-        <Link href="/posts/[id]" as='/posts/hello'><StyledListItem> 3rd post in the collection </StyledListItem></Link>
-        <Link href="/posts/[id]" as='/posts/hello'><StyledListItem> 4th post in the collection </StyledListItem></Link>
+        {/* One List Item */}
+        <ListGroup.Item className="d-flex justify-content-around">
+          <Link href="/posts/[id]" as="/posts/hello">
+            <PostName>1st post in the collection</PostName>
+          </Link>
+          <Controls>
+            <MdKeyboardArrowDown style={{color: "#007bff"}} />
+            <MdKeyboardArrowUp style={{color: "#007bff"}} />
+            <TiDeleteOutline style={{color: "#d93025"}} />
+          </Controls>
+        </ListGroup.Item>
+
+         {/* One List Item */}
+         <ListGroup.Item className="d-flex justify-content-around">
+          <Link href="/posts/[id]" as="/posts/hello">
+            <PostName>2nd post in the collection</PostName>
+          </Link>
+          <Controls>
+            <MdKeyboardArrowDown style={{color: "#007bff"}} />
+            <MdKeyboardArrowUp style={{color: "#007bff"}} />
+            <TiDeleteOutline style={{color: "#d93025"}} />
+          </Controls>
+        </ListGroup.Item>
+
+
+         {/* One List Item */}
+         <ListGroup.Item className="d-flex justify-content-around">
+          <Link href="/posts/[id]" as="/posts/hello">
+            <PostName>3rd post in the collection</PostName>
+          </Link>
+          <Controls>
+            <MdKeyboardArrowDown style={{color: "#007bff"}} />
+            <MdKeyboardArrowUp style={{color: "#007bff"}} />
+            <TiDeleteOutline style={{color: "#d93025"}} />
+          </Controls>
+        </ListGroup.Item>
+       
         <hr />
       </ListGroup>
     </Container>
