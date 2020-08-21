f9afbd0d5ec7b113a7647a1835eda43d4f62a1f9
mohamedeliwa <mohamed.av11@gmail.com>
2020-08-21 15:30:29 +0200

    initial auth functions written

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -16,6 +16,7 @@ const initialState = {
 export const AuthContext = createContext();
 
 const AuthContextProvider = (props) => {
+  const Router = useRouter();
   const [state, setState] = useState(initialState);
 
   // fetching user profile if he is authenticated
@@ -47,8 +48,102 @@ const AuthContextProvider = (props) => {
     })();
   }, []);
 
+  const signup = async (userInfo) => {
+    try {
+      // end-point for registering a new user
+      const url = "http://localhost:5000/users";
+      // sending request with the new user info
+      const respone = fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          firstName: userInfo.firstName,
+          lastName: userInfo.lastName,
+          password: userInfo.password,
+          email: userInfo.email,
+        }),
+      });
+      // checking if the user is successfully registered
+      if ((await response.status) === 201) {
+        // user object returned by the server
+        const user = await response.json();
+        setState({
+          isAuthenticated: true,
+          user: {
+            id: await user._id,
+            name: `${await user.firstName} ${await user.lastName}`,
+            email: await user.email,
+            image: await user.image,
+          },
+        });
+        Router.push("/");
+      } else {
+        throw new Error("Registeration Failed!");
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const login = async (credentials) => {
+    try {
+      // end-point for logging in
+      const url = "http://localhost:5000/users/login";
+      const response = await fetch(url, {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: credentials.email,
+          password: credentials.password,
+        }),
+      });
+
+      if ((await response.status) === 200) {
+        const user = await response.json();
+        setState({
+          isAuthenticated: true,
+          user: {
+            id: await user._id,
+            name: `${await user.firstName} ${await user.lastName}`,
+            email: await user.email,
+            image: await user.image,
+          },
+        });
+        Router.push("/");
+      } else {
+        throw new Error("Login Failed!");
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const logout = async () => {
+    try {
+      // end-point for logining out
+      const url = "http://localhost:5000/users/logout";
+      const response = await fetch(url, {
+        method: "POST",
+        credentials: "include",
+      });
+      if ((await response.status) === 200) {
+        setState(initialState);
+        Router.push("/");
+      } else {
+        throw new Error("loging out failed!");
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
-    <AuthContext.Provider value={{ ...state }}>
+    <AuthContext.Provider value={{ ...state, signup, login, logout }}>
       {props.children}
     </AuthContext.Provider>
   );
