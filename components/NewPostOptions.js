d2ceb8a3ab0ae9d6410f146bd6dfdc8445926d00
mohamedeliwa <mohamed.av11@gmail.com>
2020-08-27 21:53:52 +0200

    adding tags and excerpt input for new post options form

--- components/NewPostOptions.js
+++ components/NewPostOptions.js
@@ -1,11 +1,20 @@
 import { useState } from "react";
-import { Container, Form, Button } from "react-bootstrap";
+import {
+  Container,
+  Form,
+  Button,
+  InputGroup,
+  FormControl,
+  Badge,
+} from "react-bootstrap";
 
 const NewPostOptions = (props) => {
   const [state, setState] = useState({
     title: "",
+    excerpt: "",
     coverImage: "",
     series: "",
+    tags: [],
   });
 
   const handleChange = (e) => {
@@ -15,6 +24,12 @@ const NewPostOptions = (props) => {
         setState({
           ...state,
           title: e.target.value,
+        });
+        break;
+      case "post-excerpt":
+        setState({
+          ...state,
+          excerpt: e.target.value,
         });
         break;
       case "post-cover-image":
@@ -32,11 +47,33 @@ const NewPostOptions = (props) => {
     }
   };
 
+  const addTag = (e) => {
+    e.preventDefault();
+    const tags = state.tags;
+    const tagInput = document.querySelector("#post-tag");
+    tags.push(tagInput.value);
+    setState({
+      ...state,
+      tags,
+    });
+    tagInput.value = "";
+    console.log(state);
+  };
+
+  const postTags = state.tags.map((tag, index) => {
+    const tagVariant  = index % 2 == 0 ? "warning" : "info"; 
+    return (
+      <>
+        <Badge variant={tagVariant} key={index}>{tag}</Badge>{" "}
+      </>
+    );
+  });
+
   const handleSubmit = (e) => {
     e.preventDefault();
     props.setPost({
       ...props.post,
-      ...state
+      ...state,
     });
     props.showEditor("editor");
   };
@@ -57,6 +94,15 @@ const NewPostOptions = (props) => {
         </div>
 
         <div className="form-group">
+          <label htmlFor="post-excerpt">Post Excerpt*</label>
+          <textarea
+            className="form-control"
+            id="post-excerpt"
+            onChange={handleChange}
+          ></textarea>
+        </div>
+
+        <div className="form-group">
           <label htmlFor="post-cover-image">Post Cover Image*</label>
           <input
             type="file"
@@ -68,12 +114,27 @@ const NewPostOptions = (props) => {
         <Form.Group controlId="post-series">
           <Form.Label>Select Collection</Form.Label>
           <Form.Control as="select" defaultValue="None" onChange={handleChange}>
-            <option>None</option>
+            <option value="">None</option>
             <option>Collection1</option>
             <option>Collection2</option>
             <option>Collection3</option>
           </Form.Control>
         </Form.Group>
+
+        <Form.Group controlId="post-tag">
+          <Form.Label>Post Tags*</Form.Label>
+          <InputGroup className="mb-3">
+            <InputGroup.Prepend>
+              <Button variant="outline-secondary" onClick={addTag}>
+                Add
+              </Button>
+            </InputGroup.Prepend>
+            <FormControl aria-describedby="basic-addon1" />
+          </InputGroup>
+        </Form.Group>
+
+        <Form.Group controlId="post-tags">{postTags}</Form.Group>
+
         <Button variant="primary" type="submit">
           Continue
         </Button>
